b8455c5c8090b11f3c1915b8d4ebce76c3a4069c
Aida Abdikulova <aabdikul@Aidas-MacBook-Pro.local>
2020-03-14 19:46:32 -0400

    First Commit

---
+++ lib/look_at_book_reviews/version.rb
@@ -0,0 +1,3 @@
+module LookAtBookReviews
+  VERSION = "0.1.0"
+end
---
+++ spec/look_at_book_reviews_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe LookAtBookReviews do
+  it "has a version number" do
+    expect(LookAtBookReviews::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
