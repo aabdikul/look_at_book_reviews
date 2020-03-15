85e327f135044dfb7808c432b45d62be602d1863
Aida Abdikulova <aabdikul@Aidas-MacBook-Pro.local>
2020-03-14 20:04:12 -0400

    Removed bin and lib files as not needed.

--- lib/look_at_book_reviews.rb
+++
@@ -1,6 +0,0 @@
-require "look_at_book_reviews/version"
-
-module LookAtBookReviews
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- lib/look_at_book_reviews/version.rb
+++
@@ -1,3 +0,0 @@
-module LookAtBookReviews
-  VERSION = "0.1.0"
-end
